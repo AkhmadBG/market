c42301e1e7b56616fc1bd510a3d0c19144875872
AkhmadBG <akhmadbakir@yandex.ru>
2026-08-20 10:38:23 +0300

    refactor: add security configuration

---
+++ storefront/src/main/java/ru/project/storefront/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package ru.project.storefront.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.project.storefront.repository.UserRepository;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements ReactiveUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getUsername())
+                        .password(user.getPassword())
+                        .build());
+    }
+
+}
